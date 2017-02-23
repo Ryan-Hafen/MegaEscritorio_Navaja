678f7d0d6d193e38a37c8a7edbc8508836ca1935
Ryan-Hafen <ryan.hafen@icloud.com>
2017-02-22 17:43:02 -0800

    setup

---
+++ MegaEscritorio_Navaja/MegaEscritorio_Navaja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MegaEscritorio_Navaja.Global" Language="C#" %>
---
+++ MegaEscritorio_Navaja/MegaEscritorio_Navaja/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MegaEscritorio_Navaja.Startup))]
+namespace MegaEscritorio_Navaja
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
